837ea5add3d7a1d46468cc154c06727e853437ef
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-08-15 15:00:53 +0100

    prepare environment to use asp.net as backend server

--- __tests__/utils.spec.js
+++ __tests__/utils.spec.js
@@ -1,35 +1,32 @@
 import axios from 'axios';
-
 describe('Get a list of sentences from the API', () => {
   const baseUrl = 'http://localhost:3000';
-
   let response;
-  /**
-   * @type {Array<{ text_vo: string, translated_text: string, text_id: string}>}
-   */
-  // let data;
+  let data;
   test('Should return a 200 and have a result', async () => {
-    response = await axios.get(`${baseUrl}/api/untranslated/sentences/`);
+    response = await axios.get(`${baseUrl}/api/sentences/`);
     expect(response.status).toBe(200);
     expect(response.data).toBeTruthy();
   });
 
-  // test('Resutls from api must contain an array of data of 10 Sentences', async () => {
-  //   expect(Array.isArray(response.data.data)).toBe(true);
-  //   const { count } = response.data;
-  //   ({ data } = response.data);
-  //   expect(data.length).toBe(10);
-  //   expect(data.length).toEqual(count);
-  // });
-
-  // test('Data Resulted should have no translation', async () => {
-  //   ({ data } = response.data);
-  //   const translation = data.includes((sentence) => sentence.translated_text);
-  //   expect(translation).toBe(false);
-  // });
+  test(`Resutls must contains required fields include
+        an array of 10 phrases`, async () => {
+    expect(Array.isArray(response.data.data)).toBe(true);
+    const { count, nextCursor } = response.data;
+    ({ data } = response.data);
+    expect(count).toBe(10);
+    expect(nextCursor).toBeTruthy();
+    expect(data.length).toBe(10);
+  });
 });
 
 // describe('Posting a proposition of translation', () => {
+
+test('Data Resulted should have no translation', async () => {
+  ({ data } = response.data);
+  const translation = data.includes((sentence) => sentence.translated_text);
+  expect(translation).toBe(false);
+});
 //   const baseUrl = 'http://localhost:3000';
 //   const sentence = {
 //     idText_vo: '',
