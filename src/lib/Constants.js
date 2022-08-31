c3eeef024c285015ed94b242970239d94a977854
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-08-31 17:16:55 +0000

    fix(backendApiUrl): fix not parsed backend api url on production

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
-export const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+export const API_URL = `${API_BASE_URL}/api`;
 export const APP_NAME = process.env.NEXT_PUBLIC_APPNAME;
 export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION;
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION;
